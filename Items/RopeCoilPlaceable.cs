63001ae7e75e0679bcb388c67b76ae0a94593750
dragon3025 <11652210+dragon3025@users.noreply.github.com>
2022-10-02 10:13:29 -0700

    "Rope Coil Placeable" renamed to "Placeable Rope Coil". Changed the Pure Spirit Lamp texture (now it looks like Desert Spirit Lamp with Pearlsand coloring).

--- Items/RopeCoilPlaceable.cs
+++ Items/RopeCoilPlaceable.cs
@@ -9,7 +9,7 @@ namespace DragonsDecorativeMod.Items
     {
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Rope Coil Placeable");
+            DisplayName.SetDefault("Placeable Rope Coil");
             CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
         }
 
