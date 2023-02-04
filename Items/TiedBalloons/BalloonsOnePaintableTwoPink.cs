57988c8db1425435f59aaa0c529f36eaa020afc0
dragon3025 <11652210+dragon3025@users.noreply.github.com>
2023-02-04 14:30:21 -0800

    Balloons One Paintable With Two of a Certain Color

---
+++ Items/TiedBalloons/BalloonsOnePaintableTwoPink.cs
@@ -0,0 +1,41 @@
+using Terraria;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DragonsDecorativeMod.Items.TiedBalloons
+{
+    public class BalloonsOnePaintableTwoPink : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Balloons (2 Pink, 1 Paintable)");
+            Tooltip.SetDefault("Try painting it");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 28;
+            Item.height = 32;
+            Item.maxStack = 9999;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.rare = ItemRarityID.White;
+            Item.useAnimation = 15;
+            Item.useTime = 15;
+            Item.useStyle = ItemUseStyleID.HoldUp;
+            Item.consumable = true;
+            Item.value = Item.sellPrice(0, 0, 4);
+            Item.createTile = ModContent.TileType<Tiles.TiedBalloons.BalloonsOnePaintableTwoPink>();
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = Recipe.Create(Type);
+            recipe.AddIngredient(ItemID.PartyBundleOfBalloonTile);
+            recipe.AddIngredient(ItemID.PinkPaint, 2);
+            recipe.Register();
+        }
+    }
+}
---
+++ Tiles/TiedBalloons/BalloonsOnePaintableTwoBlack.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace DragonsDecorativeMod.Tiles.TiedBalloons
+{
+    public class BalloonsOnePaintableTwoBlack : ModTile
+    {
+        private Asset<Texture2D> overlayTexture;
+
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            Main.tileNoAttach[Type] = true;
+
+
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style2x2);
+            TileObjectData.newTile.Height = 3;
+            TileObjectData.newTile.Origin = new Point16(0, 2);
+            TileObjectData.newTile.CoordinateHeights = new int[3] { 16, 16, 16 };
+            TileObjectData.addTile(Type);
+
+            AddMapEntry(new Color(0, 0, 0));
+
+            AnimationFrameHeight = 54;
+
+            if (!Main.dedServ)
+            {
+                overlayTexture = ModContent.Request<Texture2D>("DragonsDecorativeMod/Tiles/TiedBalloons/BalloonsOnePaintableTwoBlackOverlay");
+            }
+        }
+
+        public override void KillMultiTile(int x, int y, int frameX, int frameY)
+        {
+            Item.NewItem(new EntitySource_TileBreak(x, y), x * 16, y * 16, 32, 32, ModContent.ItemType<Items.TiedBalloons.BalloonsOnePaintableTwoBlack>());
+        }
+
+        public override void AnimateTile(ref int frame, ref int frameCounter)
+        {
+            frameCounter++;
+            if (frameCounter >= 20)
+            {
+                frameCounter = 0;
+                frame++;
+                frame %= 4;
+            }
+        }
+
+        public override void PostDraw(int i, int j, SpriteBatch spriteBatch)
+        {
+            Vector2 offScreenAdjust = new(Main.offScreenRange, Main.offScreenRange);
+
+            if (Main.drawToScreen)
+            {
+                offScreenAdjust = Vector2.Zero;
+            }
+
+            Color color = Lighting.GetColor(i, j);
+
+            Tile tile = Main.tile[i, j];
+            short frameX = tile.TileFrameX;
+            short frameY = tile.TileFrameY;
+
+            // Offset along the Y axis depending on the current frame
+            int frameYOffset = Main.tileFrame[Type] * AnimationFrameHeight;
+
+            Texture2D texture = overlayTexture.Value;
+
+            spriteBatch.Draw(texture, new Vector2(i * 16 - (int)Main.screenPosition.X, j * 16 - (int)Main.screenPosition.Y) + offScreenAdjust, new Rectangle(frameX, frameY + frameYOffset, 16, 16), color, 0f, default, 1f, SpriteEffects.None, 0f);
+        }
+    }
+}
